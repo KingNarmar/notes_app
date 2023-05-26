f7cae3cb55fd7199c952e1678a93f2b9b77aa67e
KingNarmar <adlymina99@gmail.com>
2023-05-26 08:45:53 +0300

    add add note cubit to add note butom sheet

--- lib/widgets/model_bottom_sheet.dart
+++ lib/widgets/model_bottom_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
+import '../cubits/add note cubit/cubit/add_note_cubit.dart';
 import 'add_notes_form.dart';
 
 class CustomBottomSheet extends StatelessWidget {
@@ -7,10 +10,24 @@ class CustomBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: AddNotesForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print(state.errorMessage);
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: const AddNotesForm());
+          },
+        ),
       ),
     );
   }
